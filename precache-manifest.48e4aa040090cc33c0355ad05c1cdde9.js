13bbd384eec99ee7e01cb5f27ac89dc499df580b
Vadim <svanovivan@gmail.com.com>
2020-07-27 23:34:52 +0300

    Updates

---
+++ precache-manifest.48e4aa040090cc33c0355ad05c1cdde9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a88ec92a0153bca25e12d6b1a8e3269",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "6323aacd5d798784200b",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.da91dacd.chunk.css"
+  },
+  {
+    "revision": "bcda147a922a7232dbd2",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.d590b4bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.d590b4bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6323aacd5d798784200b",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.2c03975b.chunk.js"
+  },
+  {
+    "revision": "b0ee86f873d795c0676a",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.8a222599.js"
+  }
+]);
